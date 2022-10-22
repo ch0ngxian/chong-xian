8d8dcbc52f183ffab4d0eac823a70879ce036212
Chong <ch0ngx1an@hotmail.com>
2022-10-22 14:53:26 +0800

    change title and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'chong-xian',
+    title: 'Chong Xian Goh',
     htmlAttrs: {
       lang: 'en',
     },
